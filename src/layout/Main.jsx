ce3f4381ae805e999f9e1169df0a5f1467458f1d
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-02-15 17:24:47 +0600

    initialized all important file, set project name and logo

---
+++ src/layout/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h2 className='text-3xl text-green-500 ' >Hello</h2>
+        </div>
+    );
+};
+
+export default Main;
