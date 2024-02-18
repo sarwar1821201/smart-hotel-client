759d211a211989cd5b33b4a9a14adbfe1ae0defa
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-02-19 02:44:48 +0600

    show card data on the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Categories from '../Categories/Categories';
+import Rooms from '../Rooms/Rooms';
 
 const Home = () => {
     return (
         <div>
             <Categories></Categories>
+            <Rooms></Rooms>
         </div>
     );
 };
---
+++ src/pages/Rooms/Card.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import HeartButton from './HeartButton';
+
+const Card = ({room}) => {
+
+   const {location,image,dateRange,price,category} = room;
+
+    return (
+        <div className='col-span-1 cursor-pointer group'>
+        <div className='flex flex-col gap-2 w-full'>
+          <div
+            className='
+              aspect-square 
+              w-full 
+              relative 
+              overflow-hidden 
+              rounded-xl
+            '
+          >
+            <img
+              className='
+                object-cover 
+                h-full 
+                w-full 
+                group-hover:scale-110 
+                transition
+              '
+              src={image}
+              alt='Room'
+            />
+            <div
+              className='
+              absolute
+              top-3
+              right-3
+            '
+            >
+              <HeartButton />
+            </div>
+          </div>
+          <div className='font-semibold text-lg'>{location}</div>
+          <div className='font-light text-neutral-500'>
+            {dateRange}
+          </div>
+          <div className='flex flex-row items-center gap-1'>
+            <div className='font-semibold'>$ {price}</div>
+            <div className='font-light'>night</div>
+          </div>
+        </div>
+      </div>
+            
+        
+    );
+};
+
+export default Card;
